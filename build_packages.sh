6c8f0a0271adc02267607ea2b35883521b726f32
Tibor Pilz <tbrpilz@googlemail.com>
2018-08-19 10:14:14 +0200

    Added

--- build_packages.sh
+++ build_packages.sh
@@ -1,16 +1,25 @@
 #!/bin/env bash
 
-pkgbuilds="/pkg"
-repo="/repo"
-start_dir=$PWD
+pkgbuilds="pkg"
+repo="repo"
+root=$PWD
 
 cd $pkgbuilds
 
-for package in $(ls)
+for package in $(ls $root/pkg)
 do
 	cd $package
+
+	#Create package
 	makepkg -s --noconfirm
-	repo-add "$repo/lnclt.db.tar.xz" *.pkg.tar.gz
-	cp *.pkg.tar.gz "$repo/"
+
+	#Add package information to repo
+	repo-add "$root/$repo/lnclt.db.tar.xz" *.pkg.tar.gz
+
+	#Add package to repo
+	mv *.pkg.tar.gz "$root/$repo/"
+
+	#Clean up
+	rm -rf {pkg,src}
 	cd $start_dir
 done
